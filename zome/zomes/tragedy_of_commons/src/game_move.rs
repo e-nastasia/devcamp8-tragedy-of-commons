4d1655eb9e5ea1e76a0eed41d6bace55ec25e1ef
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2021-04-30 15:37:57 +0300

    game move: discoverability notes and questions

--- zome/zomes/tragedy_of_commons/src/game_move.rs
+++ zome/zomes/tragedy_of_commons/src/game_move.rs
@@ -5,30 +5,47 @@ use crate::types::ResourceAmount;
 #[hdk_entry(id = "game_move", visibility = "public")]
 pub struct GameMove {
     pub owner: AgentPubKey,
-    // PROBLEM: if we're creating the GameRound retrospectively, we can't reference it
-    // maybe add a link from GameRound -> GameMove?
-    // pub round: EntryHash,
-    // NOTE: this is similar to what we have in the new_game_round fn: every move refenreces
-    // only the previous round, and for the very first one this would be None (since rounds
-    // are created retrospectively). Having this field would make sure game moves made by the same
-    // player with the same resources value are still treated as separate entries
+    // For the very first round this option would be None, because we create game rounds
+    // retrospectively. And since all players are notified by the signal when they can make
+    // a move, maybe we could pass that value from there, so that every player has it
+    // when they're making a move
     pub previous_round: Option<EntryHashB64>,
     pub resources: ResourceAmount,
 }
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GameMoveInput {
     pub resource_amount: ResourceAmount,
+    // NOTE: if we're linking all moves to the round, this can never be None
+    // as we'll need a base for the link. Instead moves for the round 0 could be
+    // linked directly from the game session.
     pub previous_round: Option<EntryHashB64>,
 }
 
 fn new_move(input: GameMoveInput) -> () {
     // todo: calculate agent address
     // todo: create a GameMove entry
-    // note: we need to avoid querying for all game moves here because
-    // that would block the move by the player. How can we separate those two things?
-    // todo: call make_new_round to attempt to make a new round
-    // 
+    // todo: (if we're making a link from round to move) make a link round -> move
+    // note: instead of calling try_to_close_Round right here, we can have a UI make
+    // this call for us. This way making a move wouldn't be blocked by the other moves'
+    // retrieval process and the process of commiting the round entry.
 }
 
 // Question: how do we make moves discoverable by the players?
-fn get_all_round_moves() 
+// Option1: make a link from game session / game round to which this move belongs?
+//      note: this is where things start to get more complicated with the game round that is
+//      only created retrospectively. We will have to manage this duality with link base being
+//      either a game session or a game round. But maybe that's not a bad thing? That'll still
+//      be a related Holochain entry after all.
+
+// Should retrieve all game moves corresponding to the current round entry (in case of round 0 this
+// would actually be a game session entry) and attempt to close the current round by creating it's entry.
+// This would solely depend on the amount of moves retrieved being equal to the amount of players in the game
+fn try_to_close_round(current_round: EntryHash) {
+    unimplemented!()
+}
+
+// Retrieves all available game moves made in a certain round, where entry_hash identifies 
+// base for the links.
+fn get_all_round_moves(entry_hash: EntryHash) {
+    unimplemented!()
+}
