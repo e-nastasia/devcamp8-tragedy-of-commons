8231e20315196f819e41ee83b60f8578c09f5300
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2021-08-19 19:29:09 +0300

    Session get methods (#21)
    First implementation of GameSession get fns (#20)
        
    Includes fix from Guillem for versioning and integration test failures due to slow data propagation.
    Introduces the following zome functions:
    - get_my_owned_sessions for sessions created by the caller;
    - get_my_played_sessions for sessions to which caller has been invited;
    - get_all_my_sessions for all sessions caller was involved with;
    - get_my_active_sessions for all active sessions caller is currently involved with.
    Adds primitive testing of all those functions into the integration tests suite.

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
 let 
   holonixPath = builtins.fetchTarball {
     url = "https://github.com/holochain/holonix/archive/014d28000c8ed021eb84000edfe260c22e90af8c.tar.gz";
-    sha256 = "0hl5xxxjg2a6ymr44rf5dfvsb0c33dq4s6vibva6yb76yvl6gwfi";
+    sha256 = "sha256:0hl5xxxjg2a6ymr44rf5dfvsb0c33dq4s6vibva6yb76yvl6gwfi";
   };
   holonix = import (holonixPath) {
     includeHolochainBinaries = true;
     holochainVersionId = "custom";
     
     holochainVersion = { 
-     rev = "3a47f9798c6175997d27d450a7c4a0b92d17d4da";
-     sha256 = "07y9bl9q0nzwrb470walxsa8d9254mcns75hai83v1qbcjmfli4j";
-     cargoSha256 = "0sbr6ag5dyv7gdchpp5zjwzbsr1ggvxnmik21wm71gjd8hhwcdy6";
+     rev = "6b34b1797042b72aa7ae80364d3616a321924f75";
+     sha256 = "sha256:0ky2aq367ava09w19371fa77mp23kr99vp26g5gncm6nwjbazx89";
+     cargoSha256 = "sha256:07gdvccvjbg5zina751r8d8ga87pb84ss2a5ib453ykwparr53i3";
      bins = {
        holochain = "holochain";
        hc = "hc";
