31dd62c4df8a53b4637e257add8900d9fe2c5d78
tixel <tim@pragmaticmind.com>
2021-08-29 21:08:39 +0200

    somewhat working webui with mock data no conductor calls yet

--- ui/svelte-ui/src/Game.svelte
+++ ui/svelte-ui/src/Game.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { each } from "svelte/internal";
+
     import GameMove from "./GameMove.svelte";
     import GameResults from "./GameResults.svelte";
     import GameRound from "./GameRound.svelte";
@@ -7,9 +9,28 @@
     let gamecode = "3KL54M";
     let playercount = 3;
 
-    let game_status = "WAITING_PLAYERS"  // "MAKE_MOVE" "WAIT_NEXT_ROUND" "GAME_LOST" "GAME_FINISHED"
+    let game_status = "WAITING_PLAYERS"; // "MAKE_MOVE" "WAIT_NEXT_ROUND" "GAME_LOST" "GAME_FINISHED"
     function refreshPlayerList() {
         alert("refreshing");
+    }
+    function beginGame() {
+        game_status = "MAKE_MOVE";
+    }
+    let rounds = [];
+    let _round_counter = 1;
+
+    function makeMove() {
+        if (_round_counter == 2) {
+            game_status = "GAME_FINISHED";
+        } else {
+            _round_counter = _round_counter + 1;
+            rounds = [...rounds, { _round_counter }];
+            game_status = "WAIT_NEXT_ROUND";
+        }
+    }
+
+    function roundComplete() {
+        game_status = "MAKE_MOVE";
     }
 </script>
 
@@ -46,25 +67,46 @@
 
 <div class="gamerounds">
     {#if game_status == "WAITING_PLAYERS"}
-    <div class="startgame">
-        <p>
-            Wait until all player joined the game.
-            <br />
-            <a href="#" on:click={refreshPlayerList}>Refresh</a>
-            <br />
-            <button class="startgamebutton">Play!</button>
-        </p>
-    </div>
+        <div class="startgame">
+            <p>
+                Wait until all player joined the game.
+                <br />
+                <a href="#" on:click={refreshPlayerList}>Refresh</a>
+                <br />
+                <button class="startgamebutton" on:click={beginGame}
+                    >Play!</button
+                >
+            </p>
+        </div>
     {:else}
- <!-- TODO for each list of rounds played-->
-    <GameRound />
-    <GameRound />
-    <GameRound />
-
-
-    <GameMove />
-    <!-- ONLY if game ended-->
-    <GameResults />
+        <!-- TODO for each list of rounds played-->
+        {#each rounds as round, i}
+            <GameRound round={i} on:roundComplete={roundComplete} />
+        {/each}
+        {#if game_status == "MAKE_MOVE"}
+            <GameMove on:message={makeMove} />
+        {/if}
+        {#if game_status == "WAIT_NEXT_ROUND"}
+            <div style="text-align:center;">
+                Wait until round is complete...
+                <br />
+                Click refresh
+            </div>
+        {/if}
+        <!-- ONLY if game ended-->
+        {#if game_status == "GAME_LOST" || game_status == "GAME_FINISHED"}
+            {#if game_status == "GAME_LOST"}
+            <div style="text-align:center;"
+                We lost<br> What a tragedy...
+            </div>
+            {/if}
+            {#if game_status == "GAME_FINISHED"}
+            <div style="text-align:center;">
+                Yes we made it... together.
+            </div>
+            {/if}
+            <GameResults />
+        {/if}
     {/if}
 </div>
 
--- ui/svelte-ui/src/GameRound.svelte
+++ ui/svelte-ui/src/GameRound.svelte
@@ -1,21 +1,52 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    export let round = 1;
+    $: roundnum = round + 1;
+
+    let gameRoundState = "IN_PROGRESS"; // COMPLETE
+
+    function refreshGameRound() {
+        if (true) {
+            // round complete
+            gameRoundState = "COMPLETE";
+            dispatch("roundComplete", {
+                round: "1",
+            });
+        }
+    }
 </script>
 
 <section>
     <aside class="gameround">
-        <h2>Round 1</h2>
+        <h2>Round {roundnum}</h2>
         <ul>
             <li>Tixel (3216549879654321654)</li>
             <li>E-nastasia (3216549879654321654)</li>
         </ul>
-        <a style="float: right; " href="#">refresh</a>
+        {#if gameRoundState == "IN_PROGRESS"}
+            <button on:click={refreshGameRound}>refresh</button>
+        {/if}
     </aside>
 </section>
 
 <style>
-    .gameround{
+    .gameround {
         width: 70%;
         margin-top: 1rem;
         margin-bottom: 1rem;
+    }
+    button {
+        float: right;
+        background: none !important;
+        border: none;
+        padding: 0 !important;
+        /*optional*/
+        font-family: arial, sans-serif;
+        /*input has OS specific font-family*/
+        color: #118bee;
+        text-decoration: underline;
+        cursor: pointer;
     }
 </style>
