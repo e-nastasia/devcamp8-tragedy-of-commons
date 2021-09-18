0ecd71ad26ab27d63c16a86f07c151ba17172e29
tixel <tim@pragmaticmind.com>
2021-09-18 10:39:27 +0200

    increasing version to 0.0.106 important: we need to add holo_hash with encoding feature enabled otherwise B64 hashes are not available. It is probably wiser to remove all B64 and do the translation on the javascript/typescript side, like in the holochain/syn project

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
+# Example: Custom Holochain And Binaries
+# 
+# The following `shell.nix` file can be used in your project's root folder and activated with `nix-shell`.
+# It uses a custom revision and a custom set of binaries to be installed.
+
 { 
-  # https://github.com/holochain/holonix/commit/55a5eef58979fb6bc476d8c3e0c028cdeb1b5421
-  # holochain 0.0.103
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/55a5eef58979fb6bc476d8c3e0c028cdeb1b5421.tar.gz"; }
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/develop.tar.gz"; }
 }:
+
 
 let
   holonix = import (holonixPath) {
@@ -10,8 +14,29 @@ let
         # making this explicit even though it's the default
         holochainBinaries = true;
     };
-    #just using default holochain version in this holonix setup
-    holochainVersionId = "main";
+
+    holochainVersionId = "custom";
+
+    holochainVersion = {
+     rev = "b11908875a9f6a09e8939fbf6f45ff658e3d10a6";
+     sha256 = "sha256:074jdpr0dmzp2wl7flal1rdcnhd86bbf9g8dmzq4wl616v3ibqzs";
+     cargoSha256 = "sha256:0k1bbh7cklnhzkj06icbi7wamq6hl6q77d51k43qil3mrvddb7j0";
+     bins = {
+       holochain = "holochain";
+       hc = "hc";
+     };
+
+     lairKeystoreHashes = {
+       sha256 = "sha256:0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
+       cargoSha256 = "sha256:1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
+     };
+    };
   };
 
-in holonix.main
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  buildInputs = with nixpkgs; [
+    nodejs-16_x
+  ];
+}
