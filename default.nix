17a2f46381c00420357724d14fe7def23d8aef12
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-20 19:46:05 +0200

    Updated default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{ 
-  # https://github.com/holochain/holonix/commit/55a5eef58979fb6bc476d8c3e0c028cdeb1b5421
-  # holochain 0.0.103
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/55a5eef58979fb6bc476d8c3e0c028cdeb1b5421.tar.gz"; }
+# Example: Custom Holochain And Binaries
+#
+# The following `shell.nix` file can be used in your project's root folder and activated with `nix-shell`.
+# It uses a custom revision and a custom set of binaries to be installed.
+
+{
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/develop.tar.gz"; }
 }:
 
 let
@@ -10,8 +13,29 @@ let
         # making this explicit even though it's the default
         holochainBinaries = true;
     };
-    #just using default holochain version in this holonix setup
-    holochainVersionId = "main";
-  };
 
-in holonix.main
+    holochainVersionId = "custom";
+
+    holochainVersion = {
+      rev = "4bd6738d7cb2d89aa606901045669b573e8b5b99";
+      sha256 = "sha256:145f7ciibab3mzzgn9jrcj4r1fwdhbq4l9q93ghnn5yng2bbz6s4";
+      cargoSha256 = "sha256:1i8sgf1pamjzlx9p62dm81b795z8gvgcd51w42ivxwfp8jq95qrx";
+      bins = {
+        holochain = "holochain";
+        hc = "hc";
+      };
+
+      lairKeystoreHashes = {
+        sha256 = "0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
+        cargoSha256 = "1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
+      };
+    };
+  };
+  nixpkgs = holonix.pkgs;
+in nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  buildInputs = with nixpkgs; [
+    binaryen
+    nodejs-16_x
+  ];
+}
