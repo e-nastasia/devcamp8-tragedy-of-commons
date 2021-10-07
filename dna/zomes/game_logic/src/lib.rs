f072bb23670f6ae914e402bfe61f46a25ce21a5d
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2021-10-07 13:04:48 +0300

    dna: fix compilation errors

--- dna/zomes/game_logic/src/lib.rs
+++ dna/zomes/game_logic/src/lib.rs
@@ -12,9 +12,25 @@ mod game_code;
 #[allow(unused)]
 mod player_profile;
 
+use crate::player_profile::{JoinGameInfo, PlayerProfile};
+
+entry_defs![
+    player_profile::PlayerProfile::entry_def()
+];
+
 // This is another macro applied to the function that follows, and we need it to
 // expose this function as part of our backend API
 #[hdk_extern]
 pub fn create_game_code_anchor(short_unique_code: String) -> ExternResult<EntryHash> {
     game_code::create_game_code_anchor(short_unique_code)
+}
+
+#[hdk_extern]
+pub fn join_game_with_code(input: JoinGameInfo) -> ExternResult<EntryHash> {
+    player_profile::join_game_with_code(input)
+}
+
+#[hdk_extern]
+pub fn get_players_for_game_code(short_unique_code: String) -> ExternResult<Vec<PlayerProfile>> {
+    player_profile::get_player_profiles_for_game_code(short_unique_code)
 }
