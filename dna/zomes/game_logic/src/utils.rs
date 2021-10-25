a9aad363c86cd66caf3e30d79e437b1ca33505ee
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2021-10-25 10:29:54 +0400

    utils: print T name in error

--- dna/zomes/game_logic/src/utils.rs
+++ dna/zomes/game_logic/src/utils.rs
@@ -17,10 +17,7 @@ pub fn try_get_and_convert<T: TryFrom<Entry>>(entry_hash: EntryHash) -> ExternRe
 pub fn try_from_element<T: TryFrom<Entry>>(element: Element) -> ExternResult<T> {
     match element.entry() {
         element::ElementEntry::Present(entry) => {
-            // NOTE(e-nastasia): would be cool to rewrite this error to include name of T into the message
-            // I think we'd need to add more trait requirements in the fn signature to guarantee that every
-            // T has some fn to call that returns it's pretty name for messages.
-            T::try_from(entry.clone()).or(Err(WasmError::Guest(format!("Couldn't convert Element entry {:?} into provided data type", entry))))
+            T::try_from(entry.clone()).or(Err(WasmError::Guest(format!("Couldn't convert Element entry {:?} into data type {}", entry, std::any::type_name::<T>()))))
         }
         _ => Err(WasmError::Guest(format!("Element {:?} does not have an entry", element))),
     }
